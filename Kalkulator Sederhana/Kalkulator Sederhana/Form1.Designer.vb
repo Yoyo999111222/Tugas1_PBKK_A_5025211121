c8a90458e32ee531609100f42e3554b4535e9932
Yoyo999111222 <106955551+Yoyo999111222@users.noreply.github.com>
2023-09-04 16:37:43 +0700

    Add files via upload

---
+++ Kalkulator Sederhana/Kalkulator Sederhana/Form1.Designer.vb
@@ -0,0 +1,334 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TxtLayar = New System.Windows.Forms.TextBox()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnTambah = New System.Windows.Forms.Button()
+        Me.btnKurang = New System.Windows.Forms.Button()
+        Me.btnBagi = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnHasil = New System.Windows.Forms.Button()
+        Me.btnKali = New System.Windows.Forms.Button()
+        Me.btnKoma = New System.Windows.Forms.Button()
+        Me.btnPangkat = New System.Windows.Forms.Button()
+        Me.btnHapusSatu = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TxtLayar
+        '
+        Me.TxtLayar.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TxtLayar.Location = New System.Drawing.Point(12, 82)
+        Me.TxtLayar.Multiline = True
+        Me.TxtLayar.Name = "TxtLayar"
+        Me.TxtLayar.Size = New System.Drawing.Size(564, 42)
+        Me.TxtLayar.TabIndex = 0
+        Me.TxtLayar.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.Color.Silver
+        Me.btn1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn1.Location = New System.Drawing.Point(12, 147)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(109, 52)
+        Me.btn1.TabIndex = 1
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.Color.Silver
+        Me.btn2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn2.Location = New System.Drawing.Point(127, 147)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(109, 52)
+        Me.btn2.TabIndex = 2
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.Color.Silver
+        Me.btn3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn3.Location = New System.Drawing.Point(242, 147)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(109, 52)
+        Me.btn3.TabIndex = 3
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.Color.Silver
+        Me.btn4.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn4.Location = New System.Drawing.Point(12, 205)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(109, 52)
+        Me.btn4.TabIndex = 4
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.Color.Silver
+        Me.btn5.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn5.Location = New System.Drawing.Point(127, 205)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(109, 52)
+        Me.btn5.TabIndex = 5
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.Color.Silver
+        Me.btn6.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn6.Location = New System.Drawing.Point(242, 205)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(109, 52)
+        Me.btn6.TabIndex = 6
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.Color.Silver
+        Me.btn7.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn7.Location = New System.Drawing.Point(12, 263)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(109, 52)
+        Me.btn7.TabIndex = 7
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.Color.Silver
+        Me.btn8.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn8.Location = New System.Drawing.Point(127, 263)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(109, 52)
+        Me.btn8.TabIndex = 8
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.Color.Silver
+        Me.btn9.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn9.Location = New System.Drawing.Point(242, 263)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(109, 52)
+        Me.btn9.TabIndex = 9
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btnTambah
+        '
+        Me.btnTambah.BackColor = System.Drawing.Color.Silver
+        Me.btnTambah.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnTambah.Location = New System.Drawing.Point(357, 263)
+        Me.btnTambah.Name = "btnTambah"
+        Me.btnTambah.Size = New System.Drawing.Size(109, 52)
+        Me.btnTambah.TabIndex = 10
+        Me.btnTambah.Text = "+"
+        Me.btnTambah.UseVisualStyleBackColor = False
+        '
+        'btnKurang
+        '
+        Me.btnKurang.BackColor = System.Drawing.Color.Silver
+        Me.btnKurang.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnKurang.Location = New System.Drawing.Point(467, 263)
+        Me.btnKurang.Name = "btnKurang"
+        Me.btnKurang.Size = New System.Drawing.Size(109, 52)
+        Me.btnKurang.TabIndex = 11
+        Me.btnKurang.Text = "-"
+        Me.btnKurang.UseVisualStyleBackColor = False
+        '
+        'btnBagi
+        '
+        Me.btnBagi.BackColor = System.Drawing.Color.Silver
+        Me.btnBagi.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnBagi.Location = New System.Drawing.Point(467, 321)
+        Me.btnBagi.Name = "btnBagi"
+        Me.btnBagi.Size = New System.Drawing.Size(109, 52)
+        Me.btnBagi.TabIndex = 12
+        Me.btnBagi.Text = "/"
+        Me.btnBagi.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.Yellow
+        Me.btnClear.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnClear.Location = New System.Drawing.Point(357, 147)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(219, 52)
+        Me.btnClear.TabIndex = 13
+        Me.btnClear.Text = "C"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'btn0
+        '
+        Me.btn0.BackColor = System.Drawing.Color.Silver
+        Me.btn0.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn0.Location = New System.Drawing.Point(12, 321)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(339, 52)
+        Me.btn0.TabIndex = 14
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = False
+        '
+        'btnHasil
+        '
+        Me.btnHasil.BackColor = System.Drawing.Color.Silver
+        Me.btnHasil.Font = New System.Drawing.Font("Segoe UI Black", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnHasil.Location = New System.Drawing.Point(357, 379)
+        Me.btnHasil.Name = "btnHasil"
+        Me.btnHasil.Size = New System.Drawing.Size(219, 52)
+        Me.btnHasil.TabIndex = 15
+        Me.btnHasil.Text = "="
+        Me.btnHasil.UseVisualStyleBackColor = False
+        '
+        'btnKali
+        '
+        Me.btnKali.BackColor = System.Drawing.Color.Silver
+        Me.btnKali.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnKali.Location = New System.Drawing.Point(357, 321)
+        Me.btnKali.Name = "btnKali"
+        Me.btnKali.Size = New System.Drawing.Size(109, 52)
+        Me.btnKali.TabIndex = 17
+        Me.btnKali.Text = "x"
+        Me.btnKali.UseVisualStyleBackColor = False
+        '
+        'btnKoma
+        '
+        Me.btnKoma.BackColor = System.Drawing.Color.Silver
+        Me.btnKoma.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnKoma.Location = New System.Drawing.Point(242, 379)
+        Me.btnKoma.Name = "btnKoma"
+        Me.btnKoma.Size = New System.Drawing.Size(109, 52)
+        Me.btnKoma.TabIndex = 18
+        Me.btnKoma.Text = ","
+        Me.btnKoma.UseVisualStyleBackColor = False
+        '
+        'btnPangkat
+        '
+        Me.btnPangkat.BackColor = System.Drawing.Color.Silver
+        Me.btnPangkat.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnPangkat.Location = New System.Drawing.Point(12, 379)
+        Me.btnPangkat.Name = "btnPangkat"
+        Me.btnPangkat.Size = New System.Drawing.Size(224, 52)
+        Me.btnPangkat.TabIndex = 19
+        Me.btnPangkat.Text = "^"
+        Me.btnPangkat.UseVisualStyleBackColor = False
+        '
+        'btnHapusSatu
+        '
+        Me.btnHapusSatu.BackColor = System.Drawing.Color.Yellow
+        Me.btnHapusSatu.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnHapusSatu.Location = New System.Drawing.Point(357, 205)
+        Me.btnHapusSatu.Name = "btnHapusSatu"
+        Me.btnHapusSatu.Size = New System.Drawing.Size(219, 52)
+        Me.btnHapusSatu.TabIndex = 20
+        Me.btnHapusSatu.Text = "CE"
+        Me.btnHapusSatu.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 26)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(94, 27)
+        Me.PictureBox1.TabIndex = 21
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(588, 525)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnHapusSatu)
+        Me.Controls.Add(Me.btnPangkat)
+        Me.Controls.Add(Me.btnKoma)
+        Me.Controls.Add(Me.btnKali)
+        Me.Controls.Add(Me.btnHasil)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnBagi)
+        Me.Controls.Add(Me.btnKurang)
+        Me.Controls.Add(Me.btnTambah)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.TxtLayar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TxtLayar As TextBox
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnKurang As Button
+    Friend WithEvents btnBagi As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnHasil As Button
+    Friend WithEvents btnKali As Button
+    Friend WithEvents btnKoma As Button
+    Friend WithEvents btnPangkat As Button
+    Friend WithEvents btnHapusSatu As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Kalkulator Sederhana/Kalkulator Sederhana/Form1.vb
@@ -0,0 +1,167 @@
+﻿Public Class Form1
+    Friend Hasil As Single = 0
+    Dim operasi As String = ""
+    Dim OperasiHitung As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        OperasiHitung = False
+        operasi = ""
+        Hasil = 0
+        TxtLayar.Text = "0"
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        If OperasiHitung = True Or TxtLayar.Text = "0" Then
+            TxtLayar.Text = "1"
+            OperasiHitung = False
+        Else
+            TxtLayar.Text = TxtLayar.Text + "1"
+        End If
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        If OperasiHitung = True Or TxtLayar.Text = "0" Then
+            TxtLayar.Text = "2"
+            OperasiHitung = False
+        Else
+            TxtLayar.Text = TxtLayar.Text + "2"
+        End If
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        If OperasiHitung = True Or TxtLayar.Text = "0" Then
+            TxtLayar.Text = "3"
+            OperasiHitung = False
+        Else
+            TxtLayar.Text = TxtLayar.Text + "3"
+        End If
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        If OperasiHitung = True Or TxtLayar.Text = "0" Then
+            TxtLayar.Text = "4"
+            OperasiHitung = False
+        Else
+            TxtLayar.Text = TxtLayar.Text + "4"
+        End If
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        If OperasiHitung = True Or TxtLayar.Text = "0" Then
+            TxtLayar.Text = "5"
+            OperasiHitung = False
+        Else
+            TxtLayar.Text = TxtLayar.Text + "5"
+        End If
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        If OperasiHitung = True Or TxtLayar.Text = "0" Then
+            TxtLayar.Text = "6"
+            OperasiHitung = False
+        Else
+            TxtLayar.Text = TxtLayar.Text + "6"
+        End If
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        If OperasiHitung = True Or TxtLayar.Text = "0" Then
+            TxtLayar.Text = "7"
+            OperasiHitung = False
+        Else
+            TxtLayar.Text = TxtLayar.Text + "7"
+        End If
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        If OperasiHitung = True Or TxtLayar.Text = "0" Then
+            TxtLayar.Text = "8"
+            OperasiHitung = False
+        Else
+            TxtLayar.Text = TxtLayar.Text + "8"
+        End If
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        If OperasiHitung = True Or TxtLayar.Text = "0" Then
+            TxtLayar.Text = "9"
+            OperasiHitung = False
+        Else
+            TxtLayar.Text = TxtLayar.Text + "9"
+        End If
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        If OperasiHitung = True Or TxtLayar.Text = "0" Then
+            TxtLayar.Text = "0"
+            OperasiHitung = False
+        Else
+            TxtLayar.Text = TxtLayar.Text + "0"
+        End If
+    End Sub
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        operasi = "tambah"
+        Hasil = Val(TxtLayar.Text)
+        OperasiHitung = True
+    End Sub
+
+    Private Sub btnKurang_Click(sender As Object, e As EventArgs) Handles btnKurang.Click
+        operasi = "kurang"
+        Hasil = Val(TxtLayar.Text)
+        OperasiHitung = True
+    End Sub
+
+    Private Sub btnKali_Click(sender As Object, e As EventArgs) Handles btnKali.Click
+        operasi = "kali"
+        Hasil = Val(TxtLayar.Text)
+        OperasiHitung = True
+    End Sub
+
+    Private Sub btnBagi_Click(sender As Object, e As EventArgs) Handles btnBagi.Click
+        operasi = "bagi"
+        Hasil = Val(TxtLayar.Text)
+        OperasiHitung = True
+    End Sub
+
+    Private Sub btnPangkat_Click(sender As Object, e As EventArgs) Handles btnPangkat.Click
+        operasi = "pangkat"
+        Hasil = Val(TxtLayar.Text)
+        OperasiHitung = True
+    End Sub
+
+    Private Sub btnHasil_Click(sender As Object, e As EventArgs) Handles btnHasil.Click
+        Select Case operasi
+            Case "tambah"
+                Hasil = Hasil + Val(TxtLayar.Text)
+            Case "kurang"
+                Hasil = Hasil - Val(TxtLayar.Text)
+            Case "kali"
+                Hasil = Hasil * Val(TxtLayar.Text)
+            Case "bagi"
+                Hasil = Hasil / Val(TxtLayar.Text)
+            Case "pangkat"
+                Hasil = Hasil ^ Val(TxtLayar.Text)
+        End Select
+        TxtLayar.Text = Hasil
+        OperasiHitung = True
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        TxtLayar.Text = "0"
+        operasi = ""
+    End Sub
+
+    Private Sub btnHapusSatu_Click(sender As Object, e As EventArgs) Handles btnHapusSatu.Click
+        If TxtLayar.Text.Length <> 0 Then
+            TxtLayar.Text = TxtLayar.Text.Remove(TxtLayar.Text.Length - 1)
+        End If
+
+    End Sub
+
+    Private Sub btnKoma_Click(sender As Object, e As EventArgs) Handles btnKoma.Click
+        If Not TxtLayar.Text.Contains(".") Then
+            TxtLayar.Text += "."
+        End If
+    End Sub
+End Class
